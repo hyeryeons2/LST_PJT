ce744fdac971a70582b1f74a4f3596cf5535e1c6
JeesooHaa <haajeesoo@gamil.com>
2019-11-25 11:17:57 +0900

    Finish Database models
     - Make user, guild model in accounts app
     - Make movie, review model in movies app

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.conf import settings
 
-# Create your models here.
+
+class Guild(models.Model):
+    name = models.CharField(max_length=40)
+    level = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    master = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, blank=True, null=True)
+
+
+class User(AbstractUser):
+    # username = models.CharField(max_length=30, unique=True,
+    #     help_text=_()
+    # )
+    # passwrod = 
+    nickname = models.CharField(max_length=20)
+    age = models.IntegerField(blank=True, null=True)
+    followers = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name='followings' 
+    )
+    group = models.ForeignKey(Guild, related_name='member', on_delete=models.SET_NULL, blank=True, null=True)
--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Movie(models.Model):
+    title = models.CharField(max_length=50)
+    title_en = models.CharField(max_length=100)
+    audience = models.IntegerField()
+    poster_url = models.CharField(max_length=140)
+    description = models.TextField()
+    running_time = models.CharField(max_length=30)
+    directors = models.CharField(max_length=100)
+    actors = models.CharField(max_length=100)
+    watch_grade = models.CharField(max_length=30)
+    level = models.IntegerField()
+    review_link = models.CharField(max_length=200)
+    preview_link = models.CharField(max_length=200)
+    genres = models.CharField(max_length=100)
+    liked_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='liked_movies')
+    recommend_user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, blank=True, null=True)
+
+
+class Review(models.Model):
+    content = models.CharField(max_length=140)
+    score = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, blank=True, null=True, related_name='reviews')
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name='reviews')

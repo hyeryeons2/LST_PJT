0aa717d3866d2ddf57fb3e9d15b6f08a429c833a
JeesooHaa <haajeesoo@gamil.com>
2019-11-26 12:57:37 +0900

    Finish review CRUD
     - Create movie detail page
     - Add review update and delete

--- movies/urls.py
+++ movies/urls.py
@@ -6,6 +6,11 @@ app_name = 'movies'
 urlpatterns = [
     path('', views.intro, name='intro'),
     path('index/', views.index, name='index'),
+    path('<int:movie_pk>/', views.detail, name='detail'),
+    path('<int:movie_pk>/like/', views.like, name='like'),
+    path('<int:movie_pk>/reviews/', views.reviews, name='reviews'),
+    path('<int:review_pk>/reviewupdate/', views.reviewupdate, name='reviewupdate'),
+    path('<int:review_pk>/reviewdelete/', views.reviewdelete, name='reviewdelete'),
     path('addmovie/', views.addmovie, name='addmovie'),
     path('savemovie/', views.savemovie, name='savemovie'),
 ]

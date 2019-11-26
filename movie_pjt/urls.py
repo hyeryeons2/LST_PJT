ed9a5c4f1162fd1ce815739fa987bfee4ea6aadf
JeesooHaa <haajeesoo@gamil.com>
2019-11-26 18:46:08 +0900

    Create articles app
     - Make article CRUD
     - Make comments

--- movie_pjt/urls.py
+++ movie_pjt/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('movies/', include('movies.urls')),
+    path('articles/', include('articles.urls')),
     path('admin/', admin.site.urls),
 ]

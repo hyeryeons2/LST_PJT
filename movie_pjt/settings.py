0877c2cf47a129790e4e8b6be9312f67625d1ef1
Hyeryeon Kim <hyeryeons23@gmail.com>
2019-11-28 00:16:09 +0900

    Design apps using bootstrap4_3.8

--- movie_pjt/settings.py
+++ movie_pjt/settings.py
@@ -125,7 +125,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'movie_pjt', 'static'),
+    # os.path.join(BASE_DIR, 'movie_pjt', 'static'),
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 AUTH_USER_MODEL = 'accounts.User'
